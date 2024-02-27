215ba4e1c4c1d57372778f317e174d781a990020
KronosCodeur <piraliarris@gmail.com>
2024-02-27 01:53:30 +0000

    routing

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('overview');
+});
+Route::get('/customers', function () {
+    return view('Customers');
+});
+Route::get('/products', function () {
+    return view('Products');
 });
