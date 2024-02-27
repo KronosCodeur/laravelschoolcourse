c0a8335512e8e55ebcffddfd0226d93b8e3ba02b
KronosCodeur <piraliarris@gmail.com>
2024-02-27 01:53:49 +0000

    app layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -106,7 +106,7 @@
                 <ul class="space-y-3">
                     <li>
                         <a
-                            href="#"
+                            href="/"
                             class="flex items-center p-2 mt-2 text-base font-medium  rounded-lg text-white hover:bg-gray-700 group"
                         >
                             <svg
@@ -124,7 +124,7 @@
                     </li>
                     <li>
                         <a
-                            href="#"
+                            href="/customers"
                             class="flex items-center p-2 text-base font-medium  rounded-lg text-white hover:bg-gray-700 group"
                         >
                             <svg
@@ -139,7 +139,7 @@
                         </a>
                     </li><li>
                         <a
-                            href=""
+                            href="/products"
                             class="flex items-center p-2 text-base font-medium  rounded-lg text-white hover:bg-gray-700 group"
                         >
                             <svg
@@ -150,7 +150,7 @@
                                 <path d="M18 3h-5.7a2 2 0 0 0-1.4.6L3.6 11a2 2 0 0 0 0 2.8l6.6 6.6a2 2 0 0 0 2.8 0l7.4-7.5a2 2 0 0 0 .6-1.4V6a3 3 0 0 0-3-3Zm-2.4 6.4a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"/>
                             </svg>
 
-                            <span class="ml-3">Customers</span>
+                            <span class="ml-3">Products</span>
                         </a>
                     </li>
                 </ul>
@@ -159,8 +159,10 @@
 
         <main class="p-4 md:ml-64 h-auto pt-20">
             <div
-                class="border-2 border-dashed rounded-lg border-gray-600 h-[85vh] mb-4"
-            ></div>
+                class="border-2 border-dashed rounded-lg border-gray-600 h-[85vh] mb-4 text-white justify-center items-center flex"
+            >
+                @yield('content')
+            </div>
         </main>
     </div>
     </body>
